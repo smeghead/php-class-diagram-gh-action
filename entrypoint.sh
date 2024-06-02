a721c92263ec22c2da198fe3b63ffa5c92623667
smeghead <smeghead7@gmail.com>
2024-06-02 21:28:57 +0900

    specify output format.

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,8 +7,10 @@ options=$3
 repo=$GITHUB_ACTION_REPOSITORY
 branch=$GITHUB_REF_NAME
 
+format=png
 if [[ $output_path == *.svg ]]; then
 	options="$options --svg-topurl=https://github.com/$repo/blob/$target_path"
+	format=svg
 fi
 
-php-class-diagram $options ${GITHUB_WORKSPACE}/$target_path | plantuml -pipe -tpng > ${GITHUB_WORKSPACE}/${output_path}
+php-class-diagram $options ${GITHUB_WORKSPACE}/$target_path | plantuml -pipe -t${format} > ${GITHUB_WORKSPACE}/${output_path}
